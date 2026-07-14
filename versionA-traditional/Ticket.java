1e930c2179f6b062bc8f4eda13c6e45f9bc744bc
SHiranya2002 <slhiranyada@gmail.com>
2026-07-14 15:03:22 +0530

    Ticket class

---
+++ versionA-traditional/Ticket.java
@@ -0,0 +1,26 @@
+package versionA-traditional;
+
+public class Ticket {
+    private int ticketId;
+    private String ticketNo;
+    private double ticketPrice;
+
+    public Ticket(int ticketId, String ticketNo, double ticketPrice) {
+        this.ticketId = ticketId;
+        this.ticketNo = ticketNo;
+        this.ticketPrice = ticketPrice;
+    }
+
+    public int getTicketId() {
+        return ticketId;
+    }
+
+    public String getTicketNo() {
+        return ticketNo;
+    }
+
+    public double getTicketPrice() {
+        return ticketPrice;
+    }
+
+}
