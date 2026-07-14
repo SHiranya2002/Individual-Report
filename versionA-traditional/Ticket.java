5f9aae37c211a80d577df5f5e813800dad59fc31
SHiranya2002 <slhiranyada@gmail.com>
2026-07-14 16:51:10 +0530

    Ticket class update

--- versionA-traditional/Ticket.java
+++ versionA-traditional/Ticket.java
@@ -1,14 +1,11 @@
 public class Ticket {
     private int ticketId;
     private String seatNo;
-    private double ticketPrice;
-    private boolean isAvailable;
+   
 
-    public Ticket(int ticketId, String seatNo, double ticketPrice, boolean isAvailable) {
+    public Ticket(int ticketId, String seatNo) {
         this.ticketId = ticketId;
         this.seatNo = seatNo;
-        this.ticketPrice = ticketPrice;
-        this.isAvailable = isAvailable;
     }
 
     public int getTicketId() {
@@ -17,22 +14,6 @@ public class Ticket {
     
     public String getSeatNo() {
         return seatNo;
-    }
-
-    public double getTicketPrice() {
-        return ticketPrice;
-    }
-
-    public boolean isAvailable() {
-        return isAvailable;
-    }
-
-    public void reserve() {
-        isAvailable = false;
-    }
-
-    public void cancelReservation() {
-        isAvailable = true;
     }
 
 }
