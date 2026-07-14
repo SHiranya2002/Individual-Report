e99fde480c851087637c46691966c200ced8ba48
SHiranya2002 <slhiranyada@gmail.com>
2026-07-14 15:05:03 +0530

    Customer Class basic

---
+++ versionA-traditional/Customer.java
@@ -0,0 +1,32 @@
+package versionA-traditional;
+
+public class Customer {
+    private int customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhoneNo;
+
+    public Customer(int customerId, String customerName, String customerEmail, String customerPhoneNo) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.customerPhoneNo = customerPhoneNo;
+    }
+
+    public int getCustomerId() {
+        return customerId;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public String getCustomerPhoneNo() {
+        return customerPhoneNo;
+    }
+    
+}
